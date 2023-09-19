2b93b47c0d457c7fdc02fe02f778995081196426
Jaime Alejandro A. Carjaval <jaacarvajal27@gmail.com>
2023-09-19 23:30:54 +0800

    done with park, added initial unpark and fee calculation methods

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 ini_set('display_error', 1);
 
+use Carbon\Carbon;
+
 // Initalize Parking
 $parking = new App\Parking();
 
@@ -10,9 +12,19 @@ $parking = new App\Parking();
 $vehicle1 = new App\Vehicle('L');
 $vehicle2 = new App\Vehicle('M');
 $vehicle3 = new App\Vehicle('S');
+$vehicle4 = new App\Vehicle('M');
+$vehicle5 = new App\Vehicle('L');
+$vehicle6 = new App\Vehicle('S');
 
 
-$parking->park($vehicle1);
-$parking->park($vehicle2);
-$parking->park($vehicle3);
-$parking->park($vehicle4);
+$parking->park($vehicle1, 1, Carbon::now());
+$parking->park($vehicle2, 2, Carbon::now());
+$parking->park($vehicle3, 3, Carbon::now());
+$parking->park($vehicle4, 2, Carbon::now());
+$parking->park($vehicle5, 3, Carbon::now());
+$parking->park($vehicle6, 1, Carbon::now());
+
+$parking->unpark(0, 1, Carbon::now());
+
+// echo '<pre>'; print_r($parking->getTakenSlots()); echo '</pre>';
+// echo '<pre>'; print_r($parking->getEntryTimeTrackerList()); echo '</pre>';
--- src/Parking.php
+++ src/Parking.php
@@ -2,9 +2,17 @@
 
 namespace App;
 
+use Carbon\Carbon;
+
 class Parking {
-    public $vehicleTypes = ['S', 'M', 'L'];
-    public $parkingTypes = ['SP', 'MP', 'LP'];
+    const FLAT_RATE = 40;
+
+    public $takenSlots = [];
+    public $entryTimeTrackerList = [];
+    public $vehicle;
+    public $entryTime;
+    public $exitTime;
+    public $entrance;
 
     /**
      * The map of the parking slot. You are welcome to introduce a design that suits your approach. 
@@ -17,80 +25,139 @@ class Parking {
      * B -> distance from entrance 2
      * C -> distance from entrance 3
      */
-    public $map = [
+    public $parkingMap = [
         [1, 2, 3], // this array is ONE SLOT
         [3, 4, 5], // ANOTHER SLOT here...
         [2, 5, 6],
-    ];
-    /**
-     * The sizes of every corresponding parking slot. Again, you are welcome to introduce your own design. 
-     * We suggest using a list of corresponding sizes described in integers: [0, 2, 1, 1, ...] where 0, 1, 2 means small, medium, and large in that order. 
-     * Another useful design may be a dictionary of parking sizes with corresponding slots as values.
-     */
-    public $parkingSlotSize = [
-        'LP',
-        'MP',
-        'SP'
+        [4, 7, 3],
+        [5, 4, 2],
+        [2, 9, 5],
+        [3, 4, 4],
+        [5, 2, 8],
+        [7, 1, 5]
     ];
 
     /**
-     * Constructor
+     * Parking slot sizes/types
      */
-    function __construct($vehicleTypes = null, $parkingTypes = null) {
-        if ($vehicleTypes !== null) {
-            $this->vehicleTypes = $vehicleTypes;
-        }
-
-        if ($parkingTypes !== null) {
-            $this->parkingTypes = $parkingTypes;
-        }
-    }
-
-    /**
-     * Park vehicle
-     * A vehicle must be assigned a possible and available slot closest to the parking entrance
-     */
-    public function park($vehicle, ) {
-        $vehicleType = $vehicle->getType();
-        switch ($vehicleType) {
-            // Small vehicles
-            case $this->vehicleTypes[0]:
-                // if () {
-                    
-                // }
-                break;
-            // Meduim vehicles
-            case $this->vehicleTypes[1]:
-                echo 'Medium car';
-                break;
-            // Large vehicles
-            case $this->vehicleTypes[2]:
-                echo 'Chonky car';
-                break;
-            default:
-                echo 'Vehicle type is invalid';
-                break;
-        }
-    }
-
-    /**
-     * Calculate fee of a vehicle
-     */
-    private function calculateFee() {
-
-    }
-
-    /**
-     * Find the closest parking slot
-     */
-    private function findClosestSlot() {
-
-    }
+    public $parkingSlotSizes = [
+        'L',
+        'M',
+        'S',
+        'M',
+        'L',
+        'M',
+        'S',
+        'M',
+        'L'
+    ];
 
     /**
      * Getter for $type
      */
     public function getType() {
         return $this->type;
+    }
+
+    /**
+     * Getter for $type
+     */
+    public function getTakenSlots() {
+        return $this->takenSlots;
+    }
+
+    /**
+     * Getter for $type
+     */
+    public function getEntryTimeTrackerList() {
+        return $this->entryTimeTrackerList;
+    }
+
+    /**
+     * Park vehicle
+     */
+    public function park($vehicle, $entrance, $entryTime) { 
+        $this->vehicle = $vehicle;       
+        $this->entryTime = $entryTime;
+        $this->entrance = $entrance;
+
+        $this->findClosestSlot();
+    }
+
+    /**
+     * Park vehicle
+     */
+    public function unpark($parkingSlotIndex, $exitTime) {
+        $parkingSlotType = $this->parkingSlotSizes[$parkingSlotIndex]; // Get the parking slot size/type
+        $entryTime = $this->entryTimeTrackerList[$parkingSlotIndex]; // Get the entry time from $entryTimeTrackerList
+
+        // $totalTime = $exitTime - $entryTime;
+        echo self::FLAT_RATE;
+    }
+
+    /**
+     * Calculate fee of a vehicle
+     */
+    private function calculateFee() {}
+
+    /**
+     * Find the closest parking slot
+     * A vehicle must be assigned a possible and available slot closest to the parking entrance
+     */
+    private function findClosestSlot() {
+        $entranceIndex = $this->entrance - 1; // Extrance array index
+        $closestSlotDistance = PHP_INT_MAX; // Initially set the parking slot distance to max value
+        $closestSlotIndex = null; // Closest slot array index
+
+        foreach ($this->parkingMap as $index => $parkingSlotArray) {
+            $parkingSlotDistanceFromEntrance = $parkingSlotArray[$entranceIndex]; // Get the element based from $entrance
+
+            /**
+             * Check if the parking slot if closer than the previous AND
+             * If the slot is not in yet taken AND
+             * If the vehicle type is compatible with the parking slot
+             */
+            if (
+                $parkingSlotDistanceFromEntrance < $closestSlotDistance &&
+                in_array($index, $this->takenSlots) === false &&
+                $this->seeVehicleCompatibility($index) === true
+            ) {
+                $closestSlotDistance = $parkingSlotDistanceFromEntrance;
+                $closestSlotIndex = $index;
+            }            
+        }
+
+        // Add to taken slot array
+        if ($closestSlotIndex !== null) {
+            $this->takenSlots[] = $closestSlotIndex;
+            $this->entryTimeTrackerList[$closestSlotIndex] = $this->entryTime->format('Y-m-d h:i:s');
+            return;
+        }        
+    }
+
+    /**
+     * Returns true if vehicle is compatible
+     * (a) S vehicles can park in SP, MP, and LP parking spaces;
+     * (b) M vehicles can park in MP and LP parking spaces; and
+     * (c) L vehicles can park only in LP parking spaces.
+     */
+    public function seeVehicleCompatibility($index) {
+        $vehicleType = $this->vehicle->getType();
+        $parkingSlotType = $this->parkingSlotSizes[$index];
+
+        // If vehicle is small, return true
+        if ($vehicleType === 'S') {
+            return true;
+        }
+
+        if ($vehicleType === 'M' && ($parkingSlotType === 'M' || $parkingSlotType === 'L')) {
+            return true;
+        }
+
+        if ($vehicleType === 'L' && $parkingSlotType === 'L') {
+            return true;
+        }
+
+        return false;
     }
 }
